d08efa85d5dcafaf49a1a7c56d781d1fbe44f5f8
ABC <aabidshora@gmail.com>
2026-04-09 22:26:18 +0530

    prod fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
